6a13255ed60e2dacb4f38bf001033ddd677772e7
Elechim01 <68447158+Elechim01@users.noreply.github.com>
2021-06-16 22:11:40 +0200

    Initial Commit

---
+++ Shared/WeatherAppScrollingApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppScrollingApp.swift
+//  Shared
+//
+//  Created by Michele Manniello on 16/06/21.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppScrollingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
