8dd4e5257cc03fc09dec1fc12f12cbd21f94ee46
Elechim01 <68447158+Elechim01@users.noreply.github.com>
2021-06-19 23:26:37 +0200

    Rain/Snow Animation Effect

--- Shared/View/Home.swift
+++ Shared/View/Home.swift
@@ -6,10 +6,13 @@
 //
 
 import SwiftUI
-
+import SpriteKit
 struct Home: View {
     @State var offset : CGFloat = 0
     var topEge : CGFloat
+//    to avoid early starting of landing animation..
+    @State var showRain = false
+//    were going to delay start it...
     var body: some View {
         if #available(iOS 15.0, *){
         ZStack{
@@ -23,6 +26,15 @@ struct Home: View {
             .ignoresSafeArea()
 //            Blur material..
             .overlay(.ultraThinMaterial)
+//            Rain Fall View...
+//            May be its a bug...
+//            while scrolling it gets restarted...
+//            to avoid..
+            GeometryReader{_ in
+                SpriteView(scene: RainFall(),options: [.allowsTransparency])
+            }
+            .ignoresSafeArea()
+            .opacity(showRain ? 1: 0)
 //            Main View...
             ScrollView(.vertical, showsIndicators: false) {
                 VStack{
@@ -79,6 +91,16 @@ struct Home: View {
                         WeatherDataView()
 
                     }
+                    .background(
+                        GeometryReader{_ in
+                            SpriteView(scene: RainFallLanding(),options: [.allowsTransparency])
+                            .offset(y: -10)
+                        }
+                            .offset(y: -(offset + topEge) > 90 ? -(offset + (90 + topEge)): 0 )
+                            .opacity(showRain ? 1: 0)
+                    )
+                    .padding(.top,25)
+                    
                 }
                 .padding(.top,45)
                 .padding([.horizontal,.bottom])
@@ -89,15 +111,26 @@ struct Home: View {
                     let miny = proxy.frame(in: .global).minY
                     DispatchQueue.main.async {
                         self.offset = miny
+//                        why including topedge..
+//                        since we ingored top edge on main View..
+//                        print("\(minY + topEdge)")
                     }
                     return Color.clear
                     }
                 )
             }
         }
+        .onAppear {
+            DispatchQueue.main.asyncAfter(deadline: .now() + 0.8){
+                withAnimation{
+                    showRain = true
+                }
+            }
+        }
         }else{
             Text("Hola")
         }
+            
     }
     func getTitleOpacity()->CGFloat{
         let titleOffset = -getTitleOffset()
@@ -151,3 +184,44 @@ struct ForecastView: View {
         }
     }
 }
+//goign to create Rain/Snow Effect Like IOS 15 Weather App...
+//Sprite Kit Rain Scene...
+class RainFall: SKScene{
+    override func sceneDidLoad() {
+        
+        size = UIScreen.main.bounds.size
+        scaleMode = .resizeFill
+//        anchor point...
+        anchorPoint = CGPoint(x: 0.5, y: 1)
+//        bg Color...
+        backgroundColor = .clear
+        
+//        creating node and adding to scene...
+        let node = SKEmitterNode(fileNamed: "RainFall.sks")!
+        addChild(node)
+        node.particlePositionRange.dx = UIScreen.main.bounds.width
+//        Full width...
+    }
+}
+//Next Rain Fall Landing Scene...
+class RainFallLanding: SKScene{
+    override func sceneDidLoad() {
+        
+        size = UIScreen.main.bounds.size
+        scaleMode = .resizeFill
+//        anchor point...
+        let height = UIScreen.main.bounds.height
+//        getting percetage by eminiationg position range...
+        anchorPoint = CGPoint(x: 0.5, y: (height - 5)  / height)
+//        bg Color...
+        backgroundColor = .clear
+        
+//        creating node and adding to scene...
+        let node = SKEmitterNode(fileNamed: "RainFallLanding.sks")!
+        addChild(node)
+//        Removed for card padding..
+        node.particlePositionRange.dx = UIScreen.main.bounds.width - 30
+//        Full width...
+    }
+}
+
